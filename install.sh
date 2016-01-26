9d205c52142f8fd570864692f93237f892b0045f
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-01-26 15:14:22 -0200

    added osx support

--- install.sh
+++ install.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 set -eo pipefail
 
-main() {
+linux() {
   local filename="shellcheck_0.3.7-5_amd64.deb"
   wget "http://ftp.debian.org/debian/pool/main/s/shellcheck/$filename"
   sudo dpkg -i "$filename"
 }
 
-main
+osx() {
+  brew update
+  brew install shellcheck
+}
+
+if [ "$(uname -s)" = "Darwin" ]; then
+  osx
+else
+  linux
+fi
