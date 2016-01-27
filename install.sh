73b4ddf703781656b96dc45e8f8c19bd9305755e
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2016-01-26 22:10:39 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,8 @@
 set -eo pipefail
 
 linux() {
-  local filename="shellcheck_0.3.7-5_amd64.deb"
-  wget "http://ftp.debian.org/debian/pool/main/s/shellcheck/$filename"
-  sudo dpkg -i "$filename"
+  sudo apt-get update
+  sudo apt-get install -y shellcheck
 }
 
 osx() {
