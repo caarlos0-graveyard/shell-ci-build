218714f313997eabb95a77ebc13661191eaa03fa
Tim Meusel <tim@bastelfreak.de>
2016-02-05 00:20:55 +0100

    bump shellcheck version

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -eo pipefail
 
 linux() {
-  local filename="shellcheck_0.3.7-5_amd64.deb"
+  local filename="shellcheck_0.3.4-3_amd64.deb"
   wget "http://ftp.debian.org/debian/pool/main/s/shellcheck/$filename"
   sudo dpkg -i "$filename"
 }
