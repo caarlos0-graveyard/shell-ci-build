7a332e91a8b2c9ccfe9d64d27d5f1954af4656c4
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-02-08 22:53:29 -0200

    fix

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ linux() {
   sudo curl -Lso \
     /usr/bin/shellcheck \
     https://github.com/caarlos0/shellcheck-docker/releases/download/v0.4.3/shellcheck
+  sudo chmod +x /usr/bin/shellcheck
 }
 
 osx() {
